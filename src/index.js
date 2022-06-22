90a08004d05ecf0343d6fb51319399d355a64497
irinkwink <irina-ns@yandex.ru>
2022-06-22 17:54:01 +0300

    done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';              // decoded message
+    let i = 0;                    // number of symbol in the expression
+    while (i < expr.length) {
+        if (expr[i] === '*') {
+            result += ' ';        // put space when see '*'
+            i += 10;
+        } else {
+            let letterCode = '';                // letter in Morse code
+            for (let j = 0; j < 5; j++) {       //check every two symbols five times
+                switch (expr.slice(i, i + 2)) {  
+                    case '11': 
+                        letterCode += '-';
+                        break;
+                    case '10': 
+                        letterCode += '.';
+                        break;
+                }
+                i += 2;
+            }
+            result += MORSE_TABLE[letterCode];  // add decoded letter to the message
+        }
+    }
+    return result;
 }
 
 module.exports = {
